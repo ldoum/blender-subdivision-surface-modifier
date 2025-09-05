6718e8808ce275e5093d06eb8a366626d72858d4
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2025-09-05 07:08:54 -0400

    Update minimum_viable_product.py
    Added a match case block that sets subdivision type to either catmull clark or simple based on number id. Added a function that removes a modifier from a list of objects by name. that removes all modifiers. Added a new case that removes all modifiers from a list of objects.

--- minimum_viable_product.py
+++ minimum_viable_product.py
@@ -13,42 +13,81 @@ def subdivision_mod(meshy, what):
     
     more_faces = meshy.modifiers.new(name=what, type='SUBSURF')
 
-    #input block 1 - type 0 to disable, 1 to enable
+    #input 1
     more_faces.show_on_cage = bool(1)
     more_faces.show_in_editmode = bool(1)
     more_faces.show_viewport = bool(1)
     more_faces.show_render = bool(1)
 
-    #input block 2 - set type of subdivision
-    more_faces.subdivision_type = 'CATMULL_CLARK' #type 'SIMPLE' if you wish
-    more_faces.show_only_control_edges = True   #optimal display
+    #input 2
+    subdiv_type = 1
+   
+    match subdiv_type:
+            
+        case 1:
+            more_faces.subdivision_type = 'CATMULL_CLARK'
+                
+        case 2:
+            more_faces.subdivision_type = 'SIMPLE'
+            
+    #input 3      
+    more_faces.show_only_control_edges = True
 
+    #input 4
     key = 2
-    more_faces.levels = key  #viewport levels
+    
+    more_faces.levels = key
     more_faces.render_levels = key
+    
+#included this feature    
+def remove_exact_mod(unwanted, retrieve):
+    unwanted.modifiers.remove(unwanted.modifiers[retrieve])
 
         
 # Iterate over all selected objects
 for obj in bpy.context.selected_objects:
     
-    if obj.type == 'MESH':  
-        
-        #input here
-        mod_type = 'SUBSURF'
-        tag = 'subdivide seriously'
-        default = 'no'
-   
-        match mod_type:
-             
-            case 'SUBSURF':
-                
-                if default == 'yes':
-                    obj.modifiers.new(name=tag, type=mod_type)
-                else: 
-                    subdivision_mod(obj, tag)
-                        
-            case _:
-                pass
-                 
-        bpy.context.view_layer.objects.active = obj #important; adds modifier
+    """
+    option 1 to add mod, 2 for deletion for specific mod, 3 for deletion of all mods
+    """
 
+    #main input
+    option = 2
+    
+    match option:
+        
+        case 1:
+            
+            if obj.type == 'MESH':  
+        
+                #input here
+                mod_type = 'SUBSURF'
+                tag = 'subdivide seriously'
+                default = 'no'
+   
+                match mod_type:
+             
+                    case 'SUBSURF':
+                
+                        if default == 'yes':
+                            obj.modifiers.new(name=tag, type=mod_type)
+                        else: 
+                            subdivision_mod(obj, tag)
+                    case _:
+                        pass
+                 
+            bpy.context.view_layer.objects.active = obj
+            
+        case 2:
+            
+            say_name_delete = "Subdivision.001"
+            remove_exact_mod(obj, say_name_delete)
+            
+            #added the clear all mods feature
+        case 3:
+            
+            for wipe in obj.modifiers:
+                obj.modifiers.remove(wipe)
+
+        case _:
+            pass
