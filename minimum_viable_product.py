bc3eb703795e24bf184784842d52e069db85f529
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2025-09-06 01:40:06 -0400

    Update minimum_viable_product.py
    Added feature of applying all mods in a select number of objects. I also moved the line that filters for mesh type objects to the top to avoid future errors.

--- minimum_viable_product.py
+++ minimum_viable_product.py
@@ -47,27 +47,27 @@ def remove_exact_mod(unwanted, retrieve):
 # Iterate over all selected objects
 for obj in bpy.context.selected_objects:
     
-    """
-    options 
-    1 to add mod, 
-    2 for deletion for specific mod, 
-    3 for deletion of all mods, 
-    4 for applying the existing mod
-    """
+    if obj.type == 'MESH': 
+        """
+        options 
+        1 to add mod, 
+        2 for deletion for specific mod, 
+        3 for deletion of all mods, 
+        4 for applying the existing mod,
+        5 for applying all mods in many objects
+        """
 
-    #main input
-    option = 2
+        #main input
+        option = 2
 
-    match option:
+        match option:
         
-        case 1:
+            case 1:
             
-            #input here
-            mod_type = 'SUBSURF'
-            tag = 'Subdivide surface'
-            default = 'no'
-            
-            if obj.type == 'MESH':  
+                #input here
+                mod_type = 'SUBSURF'
+                tag = 'Subdivide surface'
+                default = 'no'
         
                 match mod_type:
              
@@ -80,34 +80,38 @@ for obj in bpy.context.selected_objects:
                     case _:
                         pass
                  
-            bpy.context.view_layer.objects.active = obj
+                bpy.context.view_layer.objects.active = obj
             
+            case 2:
             
-        case 2:
+                del_tag = 'Subdivide surface'
             
-            del_tag = 'Subdivide surface'
+                mod_exists = obj.modifiers.get(del_tag)
             
-            mod_exists = obj.modifiers.get(del_tag)
-            
-            if mod_exists:
-                remove_exact_mod(obj, del_tag)
-            
-            #added the clear all mods feature
-        case 3:
-            
-            for wipe in obj.modifiers:
-                obj.modifiers.remove(wipe)
-                
-        case 4:
-            apply_tag = 'Subdivide surface'
-            
-            #added the apply modifier
-            mod_is_here = obj.modifiers.get(apply_tag)
-            
-            if mod_is_here:
-                
-                bpy.context.view_layer.objects.active = obj #need this before the bottom line
-                bpy.ops.object.modifier_apply(modifier=apply_tag)
+                if mod_exists:
+                    remove_exact_mod(obj, del_tag)
 
-        case _:
-            pass
+            case 3:
+            
+                for wipe in obj.modifiers:
+                    obj.modifiers.remove(wipe)
+                
+            case 4:
+                apply_tag = 'Subdivide surface'
+            
+                #added the apply modifier
+                mod_is_here = obj.modifiers.get(apply_tag)
+            
+                if mod_is_here:
+                
+                    bpy.context.view_layer.objects.active = obj #need this before the bottom line
+                    bpy.ops.object.modifier_apply(modifier=apply_tag)
+                
+            case 5:
+                
+                for supe in obj.modifiers:
+                    bpy.context.view_layer.objects.active = obj #need this before the bottom line
+                    bpy.ops.object.modifier_apply(modifier=supe.name)
+                    
+            case _:
+                pass
